a1e46a0c83a7a261fa81b59a1c16ca0a59742985
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-06-16 19:11:56 -0300

    live 7 esquenta

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Title } from "./components/Title";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title color="red" variant="md">
+        Live de React.js do FullCycle
+      </Title>
+      <p>Contador de cliques: {count} </p>
+      <button onClick={(e) => setCount(count + 1)}>Me clique!!!</button>
+      <button onClick={(e) => setCount(0)}>Zera clique</button>
     </div>
   );
 }
